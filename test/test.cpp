7ac9b55df2eefead2dc5373153e57ef196b69b43
bupticybee <icybee@bupt.edu.cn>
2021-04-14 01:00:42 +0800

    :new: speedup by remove oppo reach prob

--- test/test.cpp
+++ test/test.cpp
@@ -524,6 +524,8 @@ TEST(TestCase,test_cfr_turn_parallel){
 }
  */
 
+
+
 TEST(TestCase,test_poker_solver){
     string ranks = "A,K,Q,J,T,9,8,7,6,5,4,3,2";
     string suits = "h,s,d,c";
@@ -535,12 +537,12 @@ TEST(TestCase,test_poker_solver){
     ps.train(
             "AA,KK,QQ,JJ,TT,99:0.75,88:0.75,77:0.5,66:0.25,55:0.25,AK,AQs,AQo:0.75,AJs,AJo:0.5,ATs:0.75,A6s:0.25,A5s:0.75,A4s:0.75,A3s:0.5,A2s:0.5,KQs,KQo:0.5,KJs,KTs:0.75,K5s:0.25,K4s:0.25,QJs:0.75,QTs:0.75,Q9s:0.5,JTs:0.75,J9s:0.75,J8s:0.75,T9s:0.75,T8s:0.75,T7s:0.75,98s:0.75,97s:0.75,96s:0.5,87s:0.75,86s:0.5,85s:0.5,76s:0.75,75s:0.5,65s:0.75,64s:0.5,54s:0.75,53s:0.5,43s:0.5",
             "QQ:0.5,JJ:0.75,TT,99,88,77,66,55,44,33,22,AKo:0.25,AQs,AQo:0.75,AJs,AJo:0.75,ATs,ATo:0.75,A9s,A8s,A7s,A6s,A5s,A4s,A3s,A2s,KQ,KJ,KTs,KTo:0.5,K9s,K8s,K7s,K6s,K5s,K4s:0.5,K3s:0.5,K2s:0.5,QJ,QTs,Q9s,Q8s,Q7s,JTs,JTo:0.5,J9s,J8s,T9s,T8s,T7s,98s,97s,96s,87s,86s,76s,75s,65s,64s,54s,53s,43s",
-             "Qs,Jh,2h",
+            "Qs,Jh,2h",
             logfile_name,
-            191,
+            91,
             10,
             "discounted_cfr",
-            100,
+            -1,
             8
     );
     //ps.dump_strategy("../resources/outputs/outputs_strategy.json");

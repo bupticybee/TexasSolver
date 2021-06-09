7b44d256426c8eebc0ebc1bf7b9b8422d0bd7820
wangfuming <wangfuming@bytedance.com>
2021-06-09 18:04:35 +0800

    :construction: debugging dump json

--- src/console.cpp
+++ src/console.cpp
@@ -1,4 +1,9 @@
 //
 // Created by bytedance on 9.6.21.
 //
+#include "tools/CommandLineTool.h"
 
+int main(int argc, char **argv) {
+    CommandLineTool clt = CommandLineTool();
+    clt.startWorking();
+}

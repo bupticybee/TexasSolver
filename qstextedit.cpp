444ea6a0068bc86029f945c12bfba1a27e43fc36
wangfuming <wangfuming@bytedance.com>
2021-09-30 12:19:46 +0800

    :new: add different macro for win/mac

--- qstextedit.cpp
+++ qstextedit.cpp
@@ -3,11 +3,15 @@
 
 QSTextEdit::QSTextEdit(QWidget *parent) : QTextEdit(parent)
 {
-    connect(this,SIGNAL(message_signal(const string&)),this,SLOT(message_slot(const string)),Qt::DirectConnection);
+    connect(this,SIGNAL(message_signal(const string&)),this,SLOT(message_slot(const string)));
 }
 
 void QSTextEdit::log_with_signal(string message){
+#if defined(Q_OS_WIN)
+    this->append(message.c_str());
+#else
     emit message_signal(message);
+#endif
 }
 
 

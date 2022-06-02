a888021619ecb4f3f952d66ce900a5d0cdd909dd
Terry Sanders <gitlab@vertone.co.uk>
2022-06-02 13:30:19 +0100

    Corrected effective stack calc inr Rule

--- src/tools/Rule.cpp
+++ src/tools/Rule.cpp
@@ -8,7 +8,7 @@ Rule::Rule(Deck deck, float oop_commit, float ip_commit, int current_round, int 
            deck(deck),oop_commit(oop_commit),ip_commit(ip_commit),current_round(current_round),raise_limit(raise_limit),
            small_blind(small_blind),big_blind(big_blind),stack(stack),build_settings(build_settings),allin_threshold(allin_threshold){
     if(oop_commit != ip_commit) throw runtime_error("ip commit should equal with oop commit");
-    this->initial_effective_stack = stack - oop_commit;
+    this->initial_effective_stack = stack;
 }
 
 float Rule::get_pot() {
